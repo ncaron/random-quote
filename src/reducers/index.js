0c51a9680ad857e598d8bf88b072e3c2e507e498
Niko <niko.caron90@gmail.com>
2017-09-24 10:26:16 -0300

    gh-pages

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import QuoteReducer from './quote_reducer';
-
-const rootReducer = combineReducers({
-  quoteData: QuoteReducer
-});
-
-export default rootReducer;
--- src/reducers/quote_reducer.js
+++
@@ -1,9 +0,0 @@
-import { FETCH_QUOTE } from '../actions/index';
-
-export default function(state = { quote: 'Loading...' }, action) {
-  switch(action.type) {
-    case FETCH_QUOTE:
-      return action.payload.data;
-  }
-  return state;
-}
